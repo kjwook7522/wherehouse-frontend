fd79101d680acba1b7f0ef465eb1d51a5d9ea801
kjwook7522 <kjwook7522@gmail.com>
2020-07-15 02:35:46 +0900

    메인 화면 검색부분

--- src/components/MainDescription/MainDescription.js
+++ src/components/MainDescription/MainDescription.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './MainDescription.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardList } from '@fortawesome/free-solid-svg-icons'
+import './MainDescription.css';
 
 function MainDescription() {
-  return <div>
-    This is Main Description
+  return <div className="description">
+    <h1>저희는 이렇습니다.</h1>
+    <div className="content">
+      <ul>
+        <li>test1</li>
+        <li>test2</li>
+        <li>test3</li>
+      </ul>
+    </div>
   </div>
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Nav.css';
+// import logo from '../../images/logo_cut.png'
 
 function Nav() {
   return <div className="nav">
-    This is Navigation
+    <div className="nav-left">
+      {/* <img src={logo} alt="logo" /> */}
+      <p>Where House logo</p>
+    </div>
+    <div className="nav-right">
+      <a href="#">회원가입</a>
+      <a href="#">로그인</a>
+    </div>
   </div>
 }
 
