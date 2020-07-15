ecfcb829de26e3fefda12ded24015c8916809e15
kjwook7522 <kjwook7522@gmail.com>
2020-07-15 16:19:52 +0900

    글자 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <h2>(주)반창고 | 대표 최윤섭 | 사업자번호: 000-00-00000</h2>
     <h2>wherehouse@gmail.com</h2>
     <h2>서울특별시 동작구 상도동 369 숭실대학교 창신관 304호</h2>
-    <h2>©WHERE HOUSE INT Co.,Ltd.</h2>
+    <h2>© WHEREHOUSE INT Co.,Ltd.</h2>
   </div>
 }
 
