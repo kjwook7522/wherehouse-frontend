ff66864bc132c1d258da79ce9fca4cedd3b3b3fd
kjwook7522 <kjwook7522@gmail.com>
2020-07-19 17:49:47 +0900

    container filter 기본 틀

--- src/components/ContainerFilter/ContainerFilter.js
+++ src/components/ContainerFilter/ContainerFilter.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Slider } from 'antd';
 import './ContainerFilter.css';
 
 function ContainerFilter() {
+  useEffect(() => {
+    //** fulfillment button selector **//
+    const allBtn = document.querySelector('#all-btn');
+    const othersBtn = document.querySelectorAll('#others-btn');
+
+    allBtn.addEventListener('click', () => {
+      othersBtn.forEach(btn => {
+        if (btn.classList.contains('active'))
+          btn.classList.remove('active');
+      });
+      allBtn.classList.add('active');
+    });
+
+    othersBtn.forEach(otherBtn => {
+      otherBtn.addEventListener('click', () => {
+        if (otherBtn.classList.contains('active'))
+          otherBtn.classList.remove('active');
+        else
+          otherBtn.classList.add('active');
+        
+        if (allBtn.classList.contains('active'))
+          allBtn.classList.remove('active');
+      });
+    });
+    //********************************//
+  }, [])
+
   return <div className='container-filter'>
-    <form>
-      <input type="text" placeholder='장소를 검색해주세요' />
-      <FontAwesomeIcon icon={faSearch} />
-    </form>
+    <div className='form-wrap'>
+      <form>
+        <input type="text" placeholder='장소를 검색해주세요' />
+        <FontAwesomeIcon icon={faSearch} />
+      </form>
+    </div>
+    <div className='fulfillment'>
+      <button className='active' id='all-btn'>All</button>
+      <button id='others-btn'>Palet</button>
+      <button id='others-btn'>Carton</button>
+      <button id='others-btn'>Unit</button>
+    </div>
+    <div style={{margin: 30}}>
+      <Slider range step={5} defaultValue={[20, 100]} marks={{0: '0m2', 100: '100m2'}} />
+    </div>
   </div>
 }
 
