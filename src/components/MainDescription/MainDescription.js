27a23228981bdbc68b608fb9f0357af5e1b396c7
kjwook7522 <kjwook7522@gmail.com>
2020-07-27 01:42:50 +0900

    Main Description 애니메이션 추가

--- src/components/MainDescription/MainDescription.js
+++ src/components/MainDescription/MainDescription.js
@@ -46,7 +46,7 @@ function MainDescription() {
       <ul className='detail-name'>
         <li className='detail-item bg-navy'>
           <img src="/images/description-image/main-des-1.jpg" alt="des-1"/>
-          <div className='detail-content'>
+          <div className='detail-content' data-aos="fade-up" data-aos-duration="800">
             <h1>3PL, 4PL 풀필먼트...</h1>
             <h1>창고 하나 찾는데 뭐가 이렇게 복잡한지...</h1>
             <h2><b>여러분에게 알맞는 최적의 물류 파트너를 지원해 드립니다.</b></h2>
@@ -56,7 +56,7 @@ function MainDescription() {
         </li>
         <li className='detail-item reverse'>
           <img src="/images/description-image/main-des-2.jpg" alt="des-2"/>
-          <div className='detail-content'>
+          <div className='detail-content' data-aos="fade-up" data-aos-duration="800">
             <h1>난 근처면 좋아! 직접 찾아보고 싶어!</h1>
             <h2><b>그렇다면, 검색하기를 통해 가장 가까운 창고를 찾아보세요.</b></h2>
             <br />
@@ -65,7 +65,7 @@ function MainDescription() {
         </li>
         <li className='detail-item bg-navy'>
           <img src="/images/description-image/main-des-3.jpg" alt="des-1"/>
-          <div className='detail-content'>
+          <div className='detail-content' data-aos="fade-up" data-aos-duration="800">
             <h1>반창고와 함께할 물류 파트너를 찾습니다.</h1>
             <h2><b>창고 등록을 통해 반창고와 함께하세요.</b></h2>
             <br />
