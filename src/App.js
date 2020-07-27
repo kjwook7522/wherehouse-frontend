5f6419773c4b1d4298a0bd54f5649ec03dce75e7
kjwook7522 <kjwook7522@gmail.com>
2020-07-27 21:06:00 +0900

    Container detail 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Main, Site } from 'pages';
+import { Main, Site, Container } from 'pages';
 import AOS from 'aos';
 import './App.css';
 
@@ -11,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Route exact path="/" component={Main} />
       <Route path="/site" component={Site} />
+      <Route path="/container" component={Container} />
     </BrowserRouter>
   );
 }
---
+++ src/pages/Container/Container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Container.css';
+
+function Container() {
+  return <div>
+    Container 입니다.
+  </div>
+}
+
+export default Container;
