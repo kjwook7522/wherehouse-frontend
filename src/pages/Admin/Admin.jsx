32f851650533324874922e0de523d5df47c00e87
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-10-07 15:03:22 +0900

    밑에폼 text타입 전송

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -7,7 +7,7 @@ class Admin extends PureComponent {
     super();
     this.state={
       header:{//이건 이름 바꿔도됨 아래는 안됨
-         headers:{Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUsImlhdCI6MTYwMTI3OTg5NiwiZXhwIjoxNjAxNjI1NDk2fQ.CNL2-KWSOu2-i1XCrVKadFpXHQljl4107dtosPFieKY'}
+         headers:{Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUsImlhdCI6MTYwMjA0NzgxNSwiZXhwIjoxNjAyMzkzNDE1fQ.g9V9YYJEf23mlVqMMJpwxN1DaNUzXB0IRwLLP2EoOfs'}
       },
     "name":" ",
     "types" : ["ROOM_TEMPERATURE"],
@@ -23,6 +23,10 @@ class Admin extends PureComponent {
     "address" : " ",
     "addressDetail" : " ",
     "description" : " ",
+    "securityCompanyName" :" ",
+    "insuranceName" : " ",
+    "parkingScale" : " ",
+    "airConditioningType" :"NONE",
   }
     this.submission=this.submission.bind(this);
     this.inputAll=this.inputAll.bind(this);
@@ -33,11 +37,13 @@ class Admin extends PureComponent {
     axios.post("/warehouses",
     {
       "name" : this.state.name,
+      "types" : this.state.types,
+      "serviceType" : this.state.serviceType,
       "landArea" : this.state.landArea,
       "totalArea" : this.state.totalArea,
       "openAt" : this.state.openAt,
       "closeAt" : this.state.closeAt,
-     // availableDays : this.state.availableDays,
+      "availableDays" : this.state.availableDays,
       "availableTimeDetail" : this.state.availableTimeDetail,
       "location" : {
           "latitude" :  this.state.latitude,
@@ -46,9 +52,12 @@ class Admin extends PureComponent {
       "address" : this.state.address,
       "addressDetail" : this.state.addressDetail,
       "description" : this.state.description,
-      "attachmentIds":[],
-      "types" : this.state.types,  //이떄 types 초기값이랑 그런거에 다 배열로 주었기 때문에 보낼떄는 다시[]로 감싸면 안되는거였음 그래서 계속 에러났던것
-      "serviceType" : this.state.serviceType,
+      
+      "securityCompanyName" :this.state.securityCompanyName,
+      "insuranceName" : this.state.insuranceName,
+      "parkingScale" : this.state.parkingScale,
+      "airConditioningType" : this.state.airConditioningType,
+      "attachmentIds":[], //이떄 types 초기값이랑 그런거에 다 배열로 주었기 때문에 보낼떄는 다시[]로 감싸면 안되는거였음 그래서 계속 에러났던것
   }
 , this.state.header)
 }
@@ -69,6 +78,10 @@ inputAll(e){
     case "address" : this.setState({address:e.target.value});break;
     case "addressDetail" : this.setState({addressDetail:e.target.value});break;
     case "description" :this.setState({description:e.target.value});break;
+    case "securityCompanyName" : this.setState({securityCompanyName:e.target.value});break;
+    case "insuranceName" : this.setState({insuranceName:e.target.value});break;
+    case "parkingScale" : this.setState({parkingScale:e.target.value});break;
+    case "airConditioningType" : this.setState({airConditioningType:e.target.value});break;
   }
 }
 
@@ -134,7 +147,7 @@ inputAll(e){
           </tr>
           <tr>
             <td><label>사용 가능한 요일</label></td>
-            <td id="availableDays" onChange={this.inputAll}>
+            <td id="availableDays" name="availableDays" onChange={this.inputAll}>
                 <label>일</label>
                 <input type="checkbox" value="1"></input>
                 <label>월</label>
@@ -190,68 +203,73 @@ inputAll(e){
           <tr>
            <td>
              <label>CCTV존재유무</label>
-            <input type="radio" name="cctv"/>있음
-            <input type="radio" name="cctv"/>없음
+            <input type="radio" name="cctv" value="true"/>있음
+            <input type="radio" name="cctv" value="false"/>없음
           </td>
         <td>
           <label>잠금장치 존재유무</label>
-          <input type="radio" name="lock"/>있음
-          <input type="radio" name="lock"/>없음
+          <input type="radio" name="lock" value="true"/>있음
+          <input type="radio" name="lock" value="false"/>없음
         </td>
           </tr>
           <tr>
            <td>
            <label>경비업체 존재유무</label>
-          <input type="radio" name="security"/>있음
-         <input type="radio" name="security"/>없음
+          <input type="radio" name="security" value="true"/>있음
+         <input type="radio" name="security" value="false"/>없음
           </td>
         <td>
         <label>경비업체 이름</label>
-        <input type="text"/>
+        <input type="text" id="securityCompanyName" name="securityCompanyName" onChange={this.inputAll}/>
         </td>
           </tr>
           <tr>
            <td>           
           <label>보험 가입유무</label>
-          <input type="radio" name="insurance"/>있음
-          <input type="radio" name="insurance"/>없음
+          <input type="radio" name="insurance" value="true"/>있음
+          <input type="radio" name="insurance" value="false"/>없음
           </td>
         <td>
         <label>보험사 이름</label>
-        <input type="text" id="insuranceName" name="insuranceName"/>
+        <input type="text" id="insuranceName" name="insuranceName" onChange={this.inputAll}/>
         </td>
           </tr>
           <tr>
            <td>
              
         <label>방문시 주차가능유무</label>
-        <input type="radio" name="park"/>있음
-        <input type="radio" name="park"/>없음
+        <input type="radio" name="park" value="true"/>있음
+        <input type="radio" name="park" value="false"/>없음
           </td>
         <td>
         <label>주차장 규모</label>
-        <input type="text" id="parkingScale" name="parkingScale"/>
+        <input type="number" id="parkingScale" name="parkingScale" onChange={this.inputAll}/>
         </td>
           </tr>
           <tr>
            <td>
            <label>관리인력 존재유무</label>
-          <input type="radio" name="worker"/>있음
-           <input type="radio" name="worker"/>없음
+          <input type="radio" name="worker" value="true"/>있음
+           <input type="radio" name="worker" value="false"/>없음
           </td>
         <td>
         <label>픽업 서비스유무</label>
-        <input type="radio" name="pickup"/>있음
-        <input type="radio" name="pickup"/>없음
+        <input type="radio" name="pickup" value="true"/>있음
+        <input type="radio" name="pickup" value="false"/>없음
         </td>
           </tr>
           <tr>
            <td>
            <label>냉난방 정보</label>
            </td>
-           <td>
-           <input type="text" id="airConditioningType" name="airConditioningType"/>
-          </td>
+           <td id="airConditioningType" name="airConditioningType" onChange={this.inputAll}>
+                <label>없음</label>
+                <input type="checkbox" value="NONE"></input>
+                <label>난방</label>
+                 <input type="checkbox" value="HEATING"></input>
+                <label>냉방</label>
+                <input type="checkbox" value="COOLING"></input>
+           </td>
           </tr>
           </tbody>
         </table>
